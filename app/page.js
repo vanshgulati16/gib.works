1e85c930121cca3ce2537e7979abd5c70758fc78
Vansh Gulati <vanshgulati16@gmail.com>
2024-08-25 18:12:20 +0530

    button hover fixed

--- app/page.js
+++ app/page.js
@@ -1,35 +1,37 @@
-
-import Head from 'next/head'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import FeaturedProjects from '@/components/FeaturedProjects'
-import Footer from '@/components/Footer'
-import Testimonials from '@/components/Testimonial'
+import Head from "next/head";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import FeaturedProjects from "@/components/FeaturedProjects";
+import Footer from "@/components/Footer";
+import Testimonials from "@/components/Testimonial";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Head>
         <title>GibWork - Collaborate on Open Source Projects</title>
-        <meta name="description" content="Join top developers shaping the future of technology through open-source collaboration" />
+        <meta
+          name="description"
+          content="Join top developers shaping the future of technology through open-source collaboration"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
 
-      <main className=''>
+      <main>
         <section>
           <Hero />
         </section>
-        <section>
+        <section >
           <FeaturedProjects />
         </section>
-        <section>
-         <Testimonials/>
+        <section >
+          <Testimonials />
         </section>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
